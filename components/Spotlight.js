020e0ea8f93867b45f7547d0b88e9bfaa50a461b
Kevin <kevinstein@gmx.net>
2024-07-09 15:29:19 +0200

    some changes

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 export default function Spotlight({ pieces }) {
   
-  const randomPiece = Math.floor(Math.random() * pieces.length);
+  const randomPiece = pieces[Math.floor(Math.random() * pieces.length)];
  
-const piece = pieces[randomPiece]
+
 
   return (
     <div>
       <Image
-        src={piece.imageSource}
-        alt={piece.name}
+        src={randomPiece.imageSource}
+        alt={randomPiece.name}
         width={400}
         height={200}
       />
-      <p>{piece.artist}</p>
+      <p>{randomPiece.artist}</p>
     </div>
   );
 }
